fdc4b1427a33d77578805410d19dbcf44fd133ea
NursenKaraulut <99021025+NursenKarabulut@users.noreply.github.com>
2022-10-23 20:40:07 +0300

    Yurt Otomasyon

---
+++ YurtKayitSistemi/YurtKayitSistemi/FrmOdemeler.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace YurtKayitSistemi
+{
+    partial class FrmOdemeler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.yurtKayitDataSet2 = new YurtKayitSistemi.YurtKayitDataSet2();
+            this.borclarBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.borclarTableAdapter = new YurtKayitSistemi.YurtKayitDataSet2TableAdapters.BorclarTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.yurtKayitDataSet3 = new YurtKayitSistemi.YurtKayitDataSet3();
+            this.borclarBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.borclarTableAdapter1 = new YurtKayitSistemi.YurtKayitDataSet3TableAdapters.BorclarTableAdapter();
+            this.ogridDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ogrAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ogrSoyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ogrKalanBorcDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.yurtKayitDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borclarBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yurtKayitDataSet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borclarBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(125, 411);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Öğrenci id:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(236, 402);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(236, 460);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 30);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(143, 469);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ödenen:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(236, 528);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(220, 30);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(118, 537);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kalan Borç:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(246, 601);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ödeme Al";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // yurtKayitDataSet2
+            // 
+            this.yurtKayitDataSet2.DataSetName = "YurtKayitDataSet2";
+            this.yurtKayitDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // borclarBindingSource
+            // 
+            this.borclarBindingSource.DataMember = "Borclar";
+            this.borclarBindingSource.DataSource = this.yurtKayitDataSet2;
+            // 
+            // borclarTableAdapter
+            // 
+            this.borclarTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ogridDataGridViewTextBoxColumn,
+            this.ogrAdDataGridViewTextBoxColumn,
+            this.ogrSoyadDataGridViewTextBoxColumn,
+            this.ogrKalanBorcDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.borclarBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(562, 369);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // yurtKayitDataSet3
+            // 
+            this.yurtKayitDataSet3.DataSetName = "YurtKayitDataSet3";
+            this.yurtKayitDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // borclarBindingSource1
+            // 
+            this.borclarBindingSource1.DataMember = "Borclar";
+            this.borclarBindingSource1.DataSource = this.yurtKayitDataSet3;
+            // 
+            // borclarTableAdapter1
+            // 
+            this.borclarTableAdapter1.ClearBeforeFill = true;
+            // 
+            // ogridDataGridViewTextBoxColumn
+            // 
+            this.ogridDataGridViewTextBoxColumn.DataPropertyName = "Ogrid";
+            this.ogridDataGridViewTextBoxColumn.HeaderText = "Ogrid";
+            this.ogridDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ogridDataGridViewTextBoxColumn.Name = "ogridDataGridViewTextBoxColumn";
+            this.ogridDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // ogrAdDataGridViewTextBoxColumn
+            // 
+            this.ogrAdDataGridViewTextBoxColumn.DataPropertyName = "OgrAd";
+            this.ogrAdDataGridViewTextBoxColumn.HeaderText = "OgrAd";
+            this.ogrAdDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ogrAdDataGridViewTextBoxColumn.Name = "ogrAdDataGridViewTextBoxColumn";
+            this.ogrAdDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // ogrSoyadDataGridViewTextBoxColumn
+            // 
+            this.ogrSoyadDataGridViewTextBoxColumn.DataPropertyName = "OgrSoyad";
+            this.ogrSoyadDataGridViewTextBoxColumn.HeaderText = "OgrSoyad";
+            this.ogrSoyadDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ogrSoyadDataGridViewTextBoxColumn.Name = "ogrSoyadDataGridViewTextBoxColumn";
+            this.ogrSoyadDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // ogrKalanBorcDataGridViewTextBoxColumn
+            // 
+            this.ogrKalanBorcDataGridViewTextBoxColumn.DataPropertyName = "OgrKalanBorc";
+            this.ogrKalanBorcDataGridViewTextBoxColumn.HeaderText = "OgrKalanBorc";
+            this.ogrKalanBorcDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ogrKalanBorcDataGridViewTextBoxColumn.Name = "ogrKalanBorcDataGridViewTextBoxColumn";
+            this.ogrKalanBorcDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // FrmOdemeler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 729);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmOdemeler";
+            this.Text = "Ödemeler";
+            this.Load += new System.EventHandler(this.FrmOdemeler_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.yurtKayitDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borclarBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yurtKayitDataSet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borclarBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private YurtKayitDataSet2 yurtKayitDataSet2;
+        private System.Windows.Forms.BindingSource borclarBindingSource;
+        private YurtKayitDataSet2TableAdapters.BorclarTableAdapter borclarTableAdapter;
+        private YurtKayitDataSet3 yurtKayitDataSet3;
+        private System.Windows.Forms.BindingSource borclarBindingSource1;
+        private YurtKayitDataSet3TableAdapters.BorclarTableAdapter borclarTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ogridDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ogrAdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ogrSoyadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ogrKalanBorcDataGridViewTextBoxColumn;
+    }
+}
---
+++ YurtKayitSistemi/YurtKayitSistemi/FrmOdemeler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YurtKayitSistemi
+{
+    public partial class FrmOdemeler : Form
+    {
+        public FrmOdemeler()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmOdemeler_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'yurtKayitDataSet3.Borclar' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.borclarTableAdapter1.Fill(this.yurtKayitDataSet3.Borclar);            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
